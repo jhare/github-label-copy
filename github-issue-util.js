4cc9bffa48f41fba917fbe5d1440005a76d790c1
Joe Hare <joseph.hare@gmail.com>
2014-04-23 11:37:50 -0500

    This thing works now

--- github-issue-util.js
+++ github-issue-util.js
@@ -4,52 +4,77 @@ var NodeRestClient = require('node-rest-client').Client;
 var RSVP = require('rsvp');
 var _ = require('lodash');
 
-function GithubIssueUtil(userName, apiToken) {
-
-  this._userName = userName;
+function GithubIssueUtil(srcUserName, apiToken, destUserName) {
+  this._srcUserName = srcUserName;
+  this._destUserName = destUserName || srcUserName;
   this._apiToken = apiToken;
 
-  this._baseUrl = 'https://api.github.com/api/repos/${owner}/repo/${repo}';
+  this._baseUrl = 'https://api.github.com/repos/${owner}/${repo}/labels';
+
+  this._defaultAuth = {
+    user: 'githubkeygoeshere',
+    password: 'x-oauth-basic'
+  };
 
   this._defaultArgs = {
-      path: {
-        owner: this._userName
-      },
       headers: {
-          'User-Agent': 'curl/7.9.8 (i686-pc-linux-gnu) libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)'
+          'User-Agent': 'curl/7.9.8 (i686-pc-linux-gnu) libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)',
+          'Accept': 'application/json'
       }
   };
 
-  this._restClient = new NodeRestClient();
+  this._makeArgs = function makeArgs(userArgs) {
+    return _.merge(this._defaultArgs, userArgs);
+  };
 
-  (function registerGithubMethods() {
-    this._restClient.registerMethod('getIssues', this._baseUrl, 'GET'); 
-    this._restClient.registerMethod('postIssues', this._baseUrl, 'POST'); 
-  })();
+  this._makeAuth = function makeAuth(srcUserName) {
+    return _.merge(this._defaultAuth, { user: srcUserName });
+  };
+
+  this._restClient = new NodeRestClient(this._makeAuth(this._apiToken));
+
+  this._restClient.registerMethod('getIssues', this._baseUrl, 'GET'); 
+  this._restClient.registerMethod('postIssue', this._baseUrl, 'POST'); 
 
   this.getIssueLabels = function getIssueLabels(srcRepo) {
     var deferred = RSVP.defer();
 
-    var args = this.makeArgs({
+    var args = this._makeArgs({
       path: {
+        owner: this._srcUserName,
         repo: srcRepo
       }
     });
 
-    this._restClient(args, function(data) {
-      deferred.resolve(data);
+    this._restClient.methods.getIssues(args, function(data) {
+      // We shouldn't have to call parse here... not sure why node-rest-client
+      // doesn't automatically do that for us.
+      deferred.resolve(JSON.parse(data));
     });
 
-    return deferred.promise();
+    return deferred.promise;
   };
 
-  this.createIssueLabel = function createIssueLabel() {
+  this.createIssueLabel = function createIssueLabel(destRepo, label) {
+    var deferred = RSVP.defer();
 
+    var args = this._makeArgs({
+      data: label,
+      path: {
+        owner: this._destUserName,
+        repo: destRepo
+      }
+    });
+
+    this._restClient.methods.postIssue(args, function(data) {
+      // We shouldn't have to call parse here... not sure why node-rest-client
+      // doesn't automatically do that for us.
+      deferred.resolve(JSON.parse(data));
+    });
+
+    return deferred.promise;
   };
 
-  this.makeArgs = function makeArgs(userArgs) {
-    return _.merge(this._defaultArgs, userArgs);
-  };
 }
 
 module.exports = GithubIssueUtil;
--- index.js
+++ index.js
@@ -3,22 +3,24 @@
  */
 'use strict';
 
+var fs = require('fs');
 var cli = require('cli');
 var _ = require('lodash');
 var GithubIssueUtil = require('./github-issue-util.js');
 
 cli.parse({
-  configFile: ['c', 'Github config file path'],
-  userName: ['u', 'User name'],
-  srcRepo: ['s', 'Source repository'],
-  destRepo: ['d', 'Destination repository']
+  configFile: ['c', 'Github config file path', 'path', '~/.githubconfig'],
+  srcUser: ['u', 'User name', 'string'],
+  destUser: [null, 'Destination user name when source/dest differ', 'string'],
+  srcRepo: ['s', 'Source repository', 'string'],
+  destRepo: ['d', 'Destination repository', 'string']
 });
 
 cli.main(function(args, options) {
   if(!options.configFile) {
     this.fatal('Must supply a Github config file. See README.md for info.');
   } else {
-    var jsonObj = JSON.parse(options.configFile);
+    var jsonObj = JSON.parse(fs.readFileSync(options.configFile));
     options.accessToken = jsonObj.personalAccessToken;
   }
 
@@ -30,14 +32,16 @@ cli.main(function(args, options) {
     this.fatal('Must supply a destination repository name.');
   }
 
-  if(!options.userName) {
+  if(!options.srcUser) {
     this.fatal('Must supply a user name.');
   }
 
-  var ghUtil = new GithubIssueUtil(options.userName, options.accessToken);
+  var ghUtil = new GithubIssueUtil(options.srcUser, options.accessToken, options.destUser);
   ghUtil.getIssueLabels(options.srcRepo).then(function(srcIssueLabels) {
+    // Iterate through the labels but leave out "url" attribute since that will
+    // have information about the source repo we don't care about anymore.
     _(srcIssueLabels).each(function(label) {
-        ghUtil.createIssueLabel(options.destRepo, label);
+      ghUtil.createIssueLabel(options.destRepo, _.omit(label, 'url'));
     });
   });
 
